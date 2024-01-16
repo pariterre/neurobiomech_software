450ca968244dc7b8f9b704093cc781f1e1c1909f
Pariterre <pariterre@hotmail.com>
2024-01-16 17:02:40 -0500

    Added a channel mock for lokomat

--- lokomat_fes/rehastim/lokomat_rehastim.py
+++ lokomat_fes/rehastim/lokomat_rehastim.py
@@ -4,14 +4,18 @@ from .rehastim_interface import RehastimInterface, Rehastim2Device
 
 class LokomatRehastim(RehastimInterface):
     def __init__(self):
+        channels = self._initialize_channels(self)
+        super().__init__(channels=channels, port="/dev/ttyUSB0", low_frequency_factor=2)
+
+    @staticmethod
+    def _initialize_channels(self) -> list[Channel]:
         channels = []
         device_type = self.device_name
         for i in range(8):
             channels.append(
                 Channel(mode=Modes.SINGLE, no_channel=i, amplitude=50, pulse_width=100, device_type=device_type)
             )
-
-        super().__init__(channels=channels, port="/dev/ttyUSB0", low_frequency_factor=2)
+        return channels
 
     @property
     def _chosen_device(self):
--- lokomat_fes/rehastim/lokomat_rehastim_mock.py
+++ lokomat_fes/rehastim/lokomat_rehastim_mock.py
@@ -15,7 +15,30 @@ class RehastimDeviceMock(RehastimDeviceAbstract):
         pass
 
 
+class ChannelMock(Channel):
+    def __init__(self, channel: Channel) -> None:
+        super(ChannelMock, self).__init__(
+            mode=channel.mode,
+            no_channel=channel.no_channel,
+            amplitude=channel.amplitude,
+            pulse_width=channel.pulse_width,
+            enable_low_frequency=channel.enable_low_frequency,
+            name=channel.name,
+            device_type=channel.device_type,
+            frequency=channel.frequency,
+            ramp=channel.ramp,
+        )
+
+    def to_sciencemode(self):
+        return self
+
+
 class LokomatRehastimMock(LokomatRehastim):
     @property
     def _chosen_device(self):
         return RehastimDeviceMock
+
+    @staticmethod
+    def _initialize_channels(self) -> list[Channel]:
+        channels = super(LokomatRehastimMock, self)._initialize_channels(self)
+        return [ChannelMock(channel) for channel in channels]
